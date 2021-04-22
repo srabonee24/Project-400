c7022df050f7a624a46cbadbb8da782c1c1b31a1
srabonee24 <76468877+srabonee24@users.noreply.github.com>
2021-04-22 13:19:44 +0600

    Add files via upload

---
+++ 414/application/views/inc/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Women's and Child Care</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+ 
+  <link href="<?=base_url()?>assets/img/favicon.png" rel="icon">
+  <link href="<?=base_url()?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+ 
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  
+  <link href="<?=base_url()?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <link href="<?=base_url()?>assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+  <header id="header" class="fixed-top  header-transparent ">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html">Women's and Child Care</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="<?=base_url()?>home">Home</a></li>
+          <li><a href="<?=base_url()?>about">App Features</a></li>
+          <li><a href="<?=base_url()?>gallery">Gallery</a></li>
+          <li><a href="<?=base_url()?>pricing">Service</a></li>
+          <li><a href="<?=base_url()?>faq">F.A.Q</a></li>
+          <li><a href="<?=base_url()?>contact">Contact Us</a></li>
+		   
+
+          <li class="get-started"><a href="<?=base_url()?>signup">Get Started</a></li>
+        </ul>
+      </nav>
+
+    </div>
+  </header>
